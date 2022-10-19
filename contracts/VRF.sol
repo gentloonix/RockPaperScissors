0f53f59382763638c970e1019e807a6190c5aabb
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-19 19:03:15 +0800

    view functions, reuse in propose & attest functions

--- contracts/VRF.sol
+++ contracts/VRF.sol
@@ -8,23 +8,31 @@ contract VRF is Ownable {
     mapping(uint256 => bytes32) public roundSecret;
 
     // === VIEWS ===
-    function calculateHash(bytes32 _secret) public view returns (bytes32) {
+    function computeHash(bytes32 _secret) public view returns (bytes32) {
         // salt (address(this)), _secret
         return keccak256(abi.encodePacked(address(this), _secret));
     }
 
+    function isRound(uint256 _round) public view returns (bool) {
+        return roundHash[_round] != bytes32(0);
+    }
+
+    function isRoundOpen(uint256 _round) public view returns (bool) {
+        return isRound(_round) && roundSecret[_round] == bytes32(0);
+    }
+
     // === MUTATIVES ===
     function propose(uint256 _round, bytes32 _hash) external onlyOwner {
-        require(roundHash[_round] == bytes32(0), "propose:: proposed");
+        require(!isRound(_round), "propose:: proposed");
 
         roundHash[_round] = _hash;
     }
 
     function attest(uint256 _round, bytes32 _secret) external onlyOwner {
-        require(roundHash[_round] != bytes32(0), "attest:: not proposed");
-        require(roundSecret[_round] == bytes32(0), "attest:: attested");
+        require(isRound(_round), "attest:: not proposed");
+        require(isRoundOpen(_round), "attest:: attested");
 
-        bytes32 _hash = calculateHash(_secret);
+        bytes32 _hash = computeHash(_secret);
         require(roundHash[_round] == _hash, "attest:: wrong secret");
         roundHash[_round] = _hash;
     }
