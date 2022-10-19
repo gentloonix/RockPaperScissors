a71ef05c456bd3a66871f3ea58185f574a9865af
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-19 19:20:51 +0800

    role control

--- contracts/VRF.sol
+++ contracts/VRF.sol
@@ -1,11 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract VRF is Ownable {
+contract VRF is AccessControl {
+    bytes32 public constant MANAGER_ROLE =
+        bytes32(uint256(keccak256("vrf.manager")) - 1);
+    bytes32 public constant PROPOSER_ROLE =
+        bytes32(uint256(keccak256("vrf.proposer")) - 1);
+    bytes32 public constant ATTESTOR_ROLE =
+        bytes32(uint256(keccak256("vrf.attestor")) - 1);
+
     mapping(uint256 => bytes32) public roundHash;
     mapping(uint256 => bytes32) public roundSecret;
+
+    constructor() {
+        _setupRole(MANAGER_ROLE, msg.sender);
+        _setupRole(PROPOSER_ROLE, msg.sender);
+        _setupRole(ATTESTOR_ROLE, msg.sender);
+    }
 
     // === VIEWS ===
     function computeHash(bytes32 _secret) public view returns (bytes32) {
@@ -21,19 +34,24 @@ contract VRF is Ownable {
         return isRound(_round) && roundSecret[_round] == bytes32(0);
     }
 
-    // === MUTATIVES ===
-    function propose(uint256 _round, bytes32 _hash) external onlyOwner {
+    // === MUTATIVES (RESTRICTED) ===
+    function propose(uint256 _round, bytes32 _hash) external {
+        require(hasRole(PROPOSER_ROLE, msg.sender), "propose:: not proposer");
+
         require(!isRound(_round), "propose:: proposed");
 
         roundHash[_round] = _hash;
     }
 
-    function attest(uint256 _round, bytes32 _secret) external onlyOwner {
+    function attest(uint256 _round, bytes32 _secret) external {
+        require(hasRole(ATTESTOR_ROLE, msg.sender), "attest:: not attestor");
+
         require(isRound(_round), "attest:: not proposed");
         require(isRoundOpen(_round), "attest:: attested");
 
         bytes32 _hash = computeHash(_secret);
         require(roundHash[_round] == _hash, "attest:: wrong secret");
+
         roundHash[_round] = _hash;
     }
 }
