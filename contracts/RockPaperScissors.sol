aa8d7f3b749ecd8da279c4bd1f3afbb3bb636ee2
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-20 09:48:12 +0800

    game basic interface placeholders

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -6,10 +6,28 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./VRF.sol";
 
 contract RockPaperScissors is Ownable {
+    struct Bet {
+        uint256 round;
+        address player_0;
+        address player_1;
+        uint256 amount;
+    }
+
     VRF public vrf;
+
+    mapping(address => Bet) public userPendingBet;
+    mapping(address => Bet) public userBet;
 
     constructor(address _vrf) {
         require(_vrf != address(0), "null vrf");
         vrf = VRF(_vrf);
     }
+
+    function depositBet(uint256 _round, address _opponent) public payable {}
+
+    function withdrawBet(uint256 _round, address _opponent) public {}
+
+    function acceptBet(uint256 _round, address _opponent) public payable {}
+
+    function declineBet(uint256 _round, address _opponent) public {}
 }
