ae69c947c0b18e4af1e91eaf79cb4c4efa8c8714
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-20 13:41:08 +0800

    Game: conclude game logic

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -9,7 +9,8 @@ contract RockPaperScissors is Ownable {
     // === STRUCTS ===
     struct Bet {
         uint256 round;
-        uint256 nonce;
+        uint256 player_0_nonce;
+        uint256 player_1_nonce;
         address player_0;
         address player_1;
         uint256 amount;
@@ -56,8 +57,60 @@ contract RockPaperScissors is Ownable {
     ) public {}
 
     function concludeGame(uint256 _round, uint256 _nonce) public {
-        Bet memory bet = userRoundNonceBet[msg.sender][_round][_nonce];
-        require(bet.block_number == 0, "concludeGame:: bet not found");
+        Bet memory player_bet = userRoundNonceBet[msg.sender][_round][_nonce];
+        require(
+            player_bet.block_number != 0,
+            "concludeGame:: player bet not found"
+        );
+        delete userRoundNonceBet[msg.sender][_round][_nonce];
+
+        uint256 player_nonce;
+        uint256 opponent_nonce;
+        address opponent;
+        if (player_bet.player_0 == msg.sender) {
+            player_nonce = player_bet.player_0_nonce;
+            opponent_nonce = player_bet.player_1_nonce;
+            opponent = player_bet.player_1;
+        } else {
+            player_nonce = player_bet.player_1_nonce;
+            opponent_nonce = player_bet.player_0_nonce;
+            opponent = player_bet.player_0;
+        }
+        address player = msg.sender;
+
+        Bet memory opponent_bet = userRoundNonceBet[opponent][_round][
+            opponent_nonce
+        ];
+        require(
+            opponent_bet.block_number != 0,
+            "concludeGame:: opponent bet not found"
+        );
+        delete userRoundNonceBet[opponent][_round][opponent_nonce];
+
+        // TODO check if both sides of bets exist (player_0 / player_1 pair)
+
+/*
+        uint256 player_choice = vrf.generate(
+            0,
+            2,
+            _round,
+            _nonce,
+            abi.encodePacked(
+                player_bet.amount,
+                blockhash(player_bet.block_number)
+            )
+        );
+        uint256 opponent_choice = vrf.generate(
+            0,
+            2,
+            _round,
+            _nonce,
+            abi.encodePacked(
+                opponent_bet.amount,
+                blockhash(opponent_bet.block_number)
+            )
+        );
+*/
 
         // TODO calculate 2 random numbers
         // TODO rock-paper-scissors logic
