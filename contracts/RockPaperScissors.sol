83732c6ab8cc931baabd5d5caf117a78897a7e96
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-20 09:31:27 +0800

    game import vrf

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RockPaperScissors is Ownable {}
+import "./VRF.sol";
+
+contract RockPaperScissors is Ownable {
+    VRF public vrf;
+
+    constructor(address _vrf) {
+        require(_vrf != address(0), "null vrf");
+        vrf = VRF(_vrf);
+    }
+}
