1be98f6c867cfdeaecbedd7bb11164bde8b276de
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-20 15:44:08 +0800

    rename functions for clearer intentions

--- contracts/VRF.sol
+++ contracts/VRF.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract VRF is AccessControl {
+    // === CONSTANTS ===
     bytes32 public constant MANAGER_ROLE =
         bytes32(uint256(keccak256("vrf.manager")) - 1);
     bytes32 public constant PROPOSER_ROLE =
@@ -11,6 +12,7 @@ contract VRF is AccessControl {
     bytes32 public constant ATTESTOR_ROLE =
         bytes32(uint256(keccak256("vrf.attestor")) - 1);
 
+    // === STATES ===
     mapping(uint256 => bytes32) public roundHash;
     mapping(uint256 => bytes32) public roundSecret;
 
@@ -26,27 +28,27 @@ contract VRF is AccessControl {
         return keccak256(abi.encodePacked(address(this), _secret));
     }
 
-    function isRound(uint256 _round) public view returns (bool) {
+    function isRoundValid(uint256 _round) public view returns (bool) {
         return roundHash[_round] != bytes32(0);
     }
 
     function isRoundOpen(uint256 _round) public view returns (bool) {
-        return isRound(_round) && roundSecret[_round] == bytes32(0);
+        return isRoundValid(_round) && roundSecret[_round] == bytes32(0);
     }
 
     // === MUTATIVES (RESTRICTED) ===
-    function propose(uint256 _round, bytes32 _hash) external {
+    function proposeRound(uint256 _round, bytes32 _hash) external {
         require(hasRole(PROPOSER_ROLE, msg.sender), "propose:: not proposer");
 
-        require(!isRound(_round), "propose:: proposed");
+        require(!isRoundValid(_round), "propose:: proposed");
 
         roundHash[_round] = _hash;
     }
 
-    function attest(uint256 _round, bytes32 _secret) external {
+    function attestRound(uint256 _round, bytes32 _secret) external {
         require(hasRole(ATTESTOR_ROLE, msg.sender), "attest:: not attestor");
 
-        require(isRound(_round), "attest:: not proposed");
+        require(isRoundValid(_round), "attest:: not proposed");
         require(isRoundOpen(_round), "attest:: attested");
 
         bytes32 _hash = computeHash(_secret);
