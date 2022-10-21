a8f0d920d70dd74b388fc8ec801e7505139e6718
Liang Xing Wong <liangxingwong@ieee.org>
2022-10-21 13:53:05 +0800

    mock

--- test/MockRockPaperScissors.sol
+++ test/MockRockPaperScissors.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MockRockPaperScissors {}
+import "../contracts/VRF.sol";
+import "../contracts/RockPaperScissors.sol";
+
+contract MockRockPaperScissors {
+    IVRF public immutable vrf;
+    RockPaperScissors public immutable game;
+
+    constructor() {
+        vrf = new VRF(address(this), address(this));
+        game = new RockPaperScissors(address(vrf));
+    }
+
+    function testAll() public {
+    }
+}
---
+++ test/MockWallet.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract MockWallet {
+    function call(
+        address target,
+        bytes memory data,
+        uint256 value
+    ) public {
+        Address.functionCallWithValue(target, data, value);
+    }
+}
